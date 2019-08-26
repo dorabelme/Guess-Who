095e43f5a9a0ae3b5781e2035d0f36855cd0afc3
deejayeaster <deejayeaster@live.com>
2019-08-26 15:16:08 -0400

    inital commit

--- guess-who/src/components/Login/Register.js
+++ guess-who/src/components/Login/Register.js
@@ -2,83 +2,88 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import { Link } from "react-router-dom";
 import * as Yup from "yup";
-import axios from "axios";
-import './login.scss';
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import "./login.scss";
 
 import Navbar from "../Navbar/Navbar";
 
 function Register({ touched, errors }) {
-    return (
-        <div className='loginPage'>
-            <Navbar />
-            <Form className="form registerForm">
-                <div className="form-box">
-                    <label className="label">Username:</label>
-                    <Field
-                        className="input"
-                        name="username"
-                        type="text"
-                        autoComplete="off"
-                    />
-                    <p>{touched.username && errors.username}</p>
-                </div>
-                <div className="form-box">
-                    <label className="label">Email:</label>
-                    <Field
-                        className="input"
-                        name="email"
-                        type="email"
-                        autoComplete="off"
-                    />
-                    <p>{touched.email && errors.email}</p>
-                </div>
-                <div className="form-box">
-                    <label className="label">Password:</label>
-                    <Field
-                        className="input"
-                        name="password"
-                        type="password"
-                        autoComplete="off"
-                    />
-                    <p>{touched.password && errors.password}</p>
-                </div>
-                <button type="submit" className="btn">
-                    Register
-                </button>
-            </Form>
-            <div className='login-redirect'>
-                <h4>By registering, you automatically accept the Terms and Policies of Guess Who app.</h4>
-                <Link className='login-link' to='/login'>Have an account? Sign In</Link>
-            </div>
+  return (
+    <div className="loginPage">
+      <Navbar />
+      <Form className="form registerForm">
+        <div className="form-box">
+          <label className="label">Username:</label>
+          <Field
+            className="input"
+            name="username"
+            type="text"
+            autoComplete="off"
+          />
+          <p>{touched.username && errors.username}</p>
         </div>
-    );
+        <div className="form-box">
+          <label className="label">Email:</label>
+          <Field
+            className="input"
+            name="email"
+            type="email"
+            autoComplete="off"
+          />
+          <p>{touched.email && errors.email}</p>
+        </div>
+        <div className="form-box">
+          <label className="label">Password:</label>
+          <Field
+            className="input"
+            name="password"
+            type="password"
+            autoComplete="off"
+          />
+          <p>{touched.password && errors.password}</p>
+        </div>
+        <button type="submit" className="btn">
+          Register
+        </button>
+      </Form>
+      <div className="login-redirect">
+        <h4>
+          By registering, you automatically accept the Terms and Policies of
+          Guess Who app.
+        </h4>
+        <Link className="login-link" to="/login">
+          Have an account? Sign In
+        </Link>
+      </div>
+    </div>
+  );
 }
 export default withFormik({
-    mapPropsToValues() {
-        return {
-            username: "",
-            password: "",
-            email: ""
-        };
-    },
-    validationSchema: Yup.object().shape({
-        username: Yup.string().required("Username is required"),
-        password: Yup.string()
-            .min(6)
-            .required("Password is required"),
-        email: Yup.string().required("Email is required")
-    }),
-    handleSubmit(values, formikBag) {
-        const url = "http://localhost:5000/api/login";
-        axios
-            .post(url, values)
-            .then(res => {
-                localStorage.setItem("token", res.data.payload);
-                formikBag.props.history.push("/bubblepage");
-            })
-            .catch(e => {
-                console.log(e.response);
-            });
-    }
+  mapPropsToValues() {
+    return {
+      username: "",
+      password: "",
+      email: ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string()
+      .min(6)
+      .required("Password is required"),
+    email: Yup.string().required("Email is required")
+  }),
+  handleSubmit(values, formikBag) {
+    const url = "https://lambda-guess-who.herokuapp.com/api/auth/register";
+    axiosWithAuth()
+      .post(url, values)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+        formikBag.props.history.push("/guesswho");
+      })
+      .catch(e => {
+        console.log(e.response);
+      });
+  }
 })(Register);
-
