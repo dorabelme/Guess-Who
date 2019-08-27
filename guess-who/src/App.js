81c4c1cbd2dcf25959b738857d8270cab3649918
Dora Belme <contact.dorabelme@gmail.com>
2019-08-27 18:04:41 -0400

    made some changes to App

--- guess-who/src/App.js
+++ guess-who/src/App.js
@@ -34,61 +34,66 @@ const initialState = {
   highScore: 0
 };
 
-// const parseToken = function parseJwt(token) {
-//   var base64Url = token.split(".")[1];
-//   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-//   var jsonPayload = decodeURIComponent(
-//     atob(base64)
-//       .split("")
-//       .map(function(c) {
-//         return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-//       })
-//       .join("")
-//   );
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
-//   return JSON.parse(jsonPayload);
-
-  function App(props) {
-    const [state, setState] = useState(initialState);
-
-    const getLogin = values => {
-      const url = "https://lambda-guess-who.herokuapp.com/api/auth/login";
-      axiosWithAuth()
-        .post(url, values)
-        .then(res => {
-          console.log("data from login:", res.config);
-          localStorage.setItem("token", res.data.token);
-          console.log("username", JSON.parse(res.config.data).username);
-          let usernameData = JSON.parse(res.config.data).username;
-          setState({ ...initialState, username: usernameData });
-          props.history.push("/guesswho");
-        })
-        .catch(e => {
-          console.log(e.response);
-        });
-    };
-
-    console.log("the set data", state);
-
-    return (
-      <div className="App">
-        <Route
-          exact
-          path="/"
-          render={props => <Login {...props} getLogin={getLogin} />}
-        />
-        <Route exact path="/register" component={Register} />
-
-        <Route path="/guesswho" component={ProtectedGuessWhoPage} />
-        <Route path="/questions" component={ProtectedQuestionList} />
-        <Route
-          path="/profile"
-          render={props => (
-            <ProtectedProfileCard {...props} username={state.username} />
-          )}
-        />
-      </div>
-    );
+  return JSON.parse(jsonPayload);
 };
+
+function App(props) {
+  const [state, setState] = useState(initialState);
+
+  const getLogin = values => {
+    const url = "https://lambda-guess-who.herokuapp.com/api/auth/login";
+    axiosWithAuth()
+      .post(url, values)
+      .then(res => {
+        console.log("data from login:", res.config);
+        localStorage.setItem("token", res.data.token);
+        console.log("username", JSON.parse(res.config.data).username);
+        let usernameData = JSON.parse(res.config.data).username;
+        setState({ ...initialState, username: usernameData });
+        let tokenData = parseJwt(res.data.token);
+        setState({ ...initialState, userId: tokenData.user.id });
+        console.log(tokenData.user.id);
+        props.history.push("/guesswho");
+      })
+      .catch(e => {
+        console.log(e.response);
+      });
+  };
+
+  console.log("the set data", state);
+  // console.log(tokenData);
+
+  return (
+    <div className="App">
+      <Route
+        exact
+        path="/"
+        render={props => <Login {...props} getLogin={getLogin} />}
+      />
+      <Route exact path="/register" component={Register} />
+
+      <Route path="/guesswho" component={ProtectedGuessWhoPage} />
+      <Route path="/questions" component={ProtectedQuestionList} />
+      <Route
+        path="/profile"
+        render={props => (
+          <ProtectedProfileCard {...props} username={state.username} />
+        )}
+      />
+    </div>
+  );
+}
 
 export default App;
