3cf04588391bb521ed344f66e8a6604655339b65
RichardEsquivel <rlesquivel@gmail.com>
2019-08-27 15:31:17 -0500

    basic icon displaying with dummy data

--- guess-who/src/components/Profile/ProfileCard.js
+++ guess-who/src/components/Profile/ProfileCard.js
@@ -2,44 +2,64 @@ import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../../utils/axiosWithAuth";
 import { Card, Image } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import "./Profile.scss";
 
-const ProfileCard = props => {
-  //state set for profile data
-  const [profile, setProfile] = useState([]);
+//Dummy Data before state is able to be passed from login
+const profile = {
+	avatar: "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
+	name: "William",
+	score: "35",
+	settings: "Gear Icon",
+	share: "Share Icon to Twitter"
+}
 
-  // State set for profile image
-  const [imageProfile, getImageProfile] = useState([]);
-  //state set for username
-  const [name, setName] = useState("name here");
-  //state set for score
-  const [score, SetScore] = useState([]);
-  //state set for changing user settings
-  const [settings, getSettings] = useState([]);
+function ProfileCard() {
 
-  /* 
-  
-  
 
-  */
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get("https://lambda-guess-who.herokuapp.com/api/auth/login")
-      .then(response => {
-        setProfile(response.data);
-        console.log("DataINeed", response);
-      })
-      .catch(error => {
-        console.log(error.response);
-      });
-  }, []);
+	// const ProfileCard = props => {
+	//   //state set for profile data
+	//   const [profile, setProfile] = useState([]);
 
-  return (
-    <div className="profile-card">
-      {profile.map(profiles => {
+	//   // State set for profile image
+	//   const [avatar, getAvatar] = useState([]);
+	//   //state set for username
+	//   const [name, setName] = useState("name here");
+	//   //state set for score
+	//   const [score, SetScore] = useState([]);
+	//   //state set for changing user settings
+	//   const [settings, getSettings] = useState([]);
+
+	//   useEffect(() => {
+	//     axiosWithAuth()
+	//       .get("https://lambda-guess-who.herokuapp.com/api/auth/login")
+	//       .then(response => {
+	//         setProfile(response.data);
+	//         console.log("DataINeed", response);
+	//       })
+	//       .catch(error => {
+	//         console.log(error.response);
+	//       });
+	//   }, []);
+
+	return (
+		<div className="profile-card">
+			{/* {profile.map(profiles => {
         return <ProfileCard key={profiles.config} profiles={profiles} />;
-      })}
-    </div>
-  );
-};
+	  })} 
+	 }; 
+	  */}
+			<Card className=".ui.card">
+				<Image
+					src={profile.avatar}
+				/>
+				<Card.Content>
+					<Card.Header>{profile.name}</Card.Header>
+
+				</Card.Content>
+			</Card>
+
+		</div>
+	);
+}
 export default ProfileCard;
