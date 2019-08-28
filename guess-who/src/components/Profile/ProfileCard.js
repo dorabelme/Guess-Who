ceeebe092d6a14368d6ed85269b391fcb5e8de0e
Dora Belme <contact.dorabelme@gmail.com>
2019-08-28 12:05:37 -0400

    merged profileCard from Richard

--- guess-who/src/components/Profile/ProfileCard.js
+++ guess-who/src/components/Profile/ProfileCard.js
@@ -6,87 +6,79 @@ import "semantic-ui-css/semantic.min.css";
 import "./Profile.scss";
 
 //Dummy Data before state is able to be passed from login
-	const profile = {
+const profile = {
 	avatar: "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
 	header: "My Profile: ",
 	description:
-			'I want to tweet the very best. '
-	
-		
-	  
-// 	//name: "William",
-// 	// score: "35",
-// 	// settings: "Settings",
-// 	share: "Share Icon to Twitter"
- }
-
-<<<<<<< HEAD
-=======
-function ProfileCard({username, highScore}) {
->>>>>>> b4894892a5e91dca38a0c0d950083ca8e4bd5d06
+		'I want to tweet the very best. '
 
 
 
+	// 	//name: "William",
+	// 	// score: "35",
+	// 	// settings: "Settings",
+	// 	share: "Share Icon to Twitter"
+}
+
+
+function ProfileCard({ username, highScore }) {
+		// const [avatar, getAvatar] = useState({})
+
+		highScore = highScore + 3;
+		const newScores = highScore * 10;
 
 
 
-
-export default function ProfileCard({username, highScore}) {
-// const [avatar, getAvatar] = useState({})
-
-	highScore= highScore + 3;
-	const newScores = highScore * 10;
-
-	
-
-// 	const yourAvatar = () => {
-// 		axios()
-// 		.get("https://dog.ceo/api/breeds/image/random/")
-// 		  .then(response => {
-// 			getAvatar(response.data);
-// 			console.log("DataINeed", response);
-// 		  })
-// 		  .catch(error => {
-// 			console.log(error.message);
-// 		  });
-// 		  yourAvatar();
-// 	  }
-	// const ProfileCard = props => {
-	//   //state set for profile data
-	//   const [profile, setProfile] = useState([]);
-	// State set for profile image
-	//   //state set for username
-	//   const [name, setName] = useState("name here");
-	//   //state set for score
-	//   const [score, SetScore] = useState([]);
-	//   //state set for changing user settings
-	//   const [settings, getSettings] = useState([]);
-return (
-		<>
-{/* {profile.map(profiles => {
+		// 	const yourAvatar = () => {
+		// 		axios()
+		// 		.get("https://dog.ceo/api/breeds/image/random/")
+		// 		  .then(response => {
+		// 			getAvatar(response.data);
+		// 			console.log("DataINeed", response);
+		// 		  })
+		// 		  .catch(error => {
+		// 			console.log(error.message);
+		// 		  });
+		// 		  yourAvatar();
+		// 	  }
+		// const ProfileCard = props => {
+		//   //state set for profile data
+		//   const [profile, setProfile] = useState([]);
+		// State set for profile image
+		//   //state set for username
+		//   const [name, setName] = useState("name here");
+		//   //state set for score
+		//   const [score, SetScore] = useState([]);
+		//   //state set for changing user settings
+		//   const [settings, getSettings] = useState([]);
+		return (
+			<>
+				{/* {profile.map(profiles => {
         return <ProfileCard key={profiles.config} profiles={profiles} />;
 	  })} 
 	 }; 
 	  */}
-	<div className="profile-card">
-		<Card>
-			<Image src={profile.avatar}/>
-			<Card.Content>
-				<Card.Header><h1 className="userName">{username}</h1></Card.Header>
-				<h2>{profile.header}</h2>
-		<div class="container">
-		<div className="skills scores">{newScores}</div>
-</div>
+				<div className="profile-card">
+					<Card>
+						<Image src={profile.avatar} />
+						<Card.Content>
+							<Card.Header><h1 className="userName">{username}</h1></Card.Header>
+							<h2>{profile.header}</h2>
+							<div class="container">
+								<div className="skills scores">{newScores}</div>
+							</div>
 
-				<h2>{newScores} Tweety Points!</h2>
-				{/*profile.settings*/}
-				<h3>{100 - newScores} points till the next level!</h3>
-				<h3>About me: {profile.description}</h3>
-			</Card.Content>
-		</Card>
-	</div>
-	</>
+							<h2>{newScores} Tweety Points!</h2>
+							{/*profile.settings*/}
+							<h3>{100 - newScores} points till the next level!</h3>
+							<h3>About me: {profile.description}</h3>
+						</Card.Content>
+					</Card>
+				</div>
+			</>
 
 
-	);
+		);
 	}
+
+export default ProfileCard;
