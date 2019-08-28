8a3b07d9e4664cfbe3050048517064e605867ed5
RichardEsquivel <rlesquivel@gmail.com>
2019-08-28 08:54:16 -0500

    styling profile card

--- guess-who/src/components/Profile/ProfileCard.js
+++ guess-who/src/components/Profile/ProfileCard.js
@@ -1,70 +1,71 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { axiosWithAuth } from "../../utils/axiosWithAuth";
 import { Card, Image } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./Profile.scss";
 
 //Dummy Data before state is able to be passed from login
-const profile = {
-	avatar: "https://images.dog.ceo/breeds/whippet/n02091134_39.jpg",
-	name: "William",
-	score: "35",
-	settings: "Settings",
-	share: "Share Icon to Twitter"
-}
-
-function ProfileCard() {
+	const profile = {
+	avatar: "https://react.semantic-ui.com/images/avatar/large/elliot.jpg"
+// 	//name: "William",
+// 	// score: "35",
+// 	// settings: "Settings",
+// 	share: "Share Icon to Twitter"
+ }
 
 
 
+
+
+
+
+
+export default function ProfileCard({username, highScore}) {
+// const [avatar, getAvatar] = useState({})
+
+// 	const yourAvatar = () => {
+// 		axios()
+// 		.get("https://dog.ceo/api/breeds/image/random/")
+// 		  .then(response => {
+// 			getAvatar(response.data);
+// 			console.log("DataINeed", response);
+// 		  })
+// 		  .catch(error => {
+// 			console.log(error.message);
+// 		  });
+// 		  yourAvatar();
+// 	  }
 	// const ProfileCard = props => {
 	//   //state set for profile data
 	//   const [profile, setProfile] = useState([]);
-
-	//   // State set for profile image
-	//   const [avatar, getAvatar] = useState([]);
+	// State set for profile image
 	//   //state set for username
 	//   const [name, setName] = useState("name here");
 	//   //state set for score
 	//   const [score, SetScore] = useState([]);
 	//   //state set for changing user settings
 	//   const [settings, getSettings] = useState([]);
-
-	//   useEffect(() => {
-	//     axiosWithAuth()
-	//       .get("https://lambda-guess-who.herokuapp.com/api/auth/login")
-	//       .then(response => {
-	//         setProfile(response.data);
-	//         console.log("DataINeed", response);
-	//       })
-	//       .catch(error => {
-	//         console.log(error.response);
-	//       });
-	//   }, []);
-
-	return (
-		<div className="profile-card">
-
-			{/* {profile.map(profiles => {
+return (
+		<>
+{/* {profile.map(profiles => {
         return <ProfileCard key={profiles.config} profiles={profiles} />;
 	  })} 
 	 }; 
 	  */}
-			<Card>
-				<Image
-					src={profile.avatar}
-
-				/>
-				<Card.Content>
-					<Card.Header>{profile.name}</Card.Header>
-					<h2>{profile.score}</h2>
-					{profile.settings}
-					<h1>Share to Twitter</h1>
-				</Card.Content>
-			</Card>
-		</div>
+	<div className="profile-card">
+		<Card>
+			<Image src={profile.avatar}/>
+			<Card.Content>
+				<Card.Header><h1>{username}</h1></Card.Header>
+				<h2>{highScore}</h2>
+				{/*profile.settings*/}
+				<h1>Share to Twitter</h1>
+			</Card.Content>
+		</Card>
+	</div>
+	</>
 
 
 	);
-}
-export default ProfileCard;
+	}
