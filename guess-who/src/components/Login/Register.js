1c9f3a7c5fb4f66a57b04033739e74be2e636107
deejayeaster <deejayeaster@live.com>
2019-08-29 18:52:40 -0400

    fixed background bug

--- guess-who/src/components/Login/Register.js
+++ guess-who/src/components/Login/Register.js
@@ -32,6 +32,16 @@ function Register({ touched, errors }) {
           />
           <p>{touched.password && errors.password}</p>
         </div>
+        <div className="form-box">
+          <label className="label">Confirm:</label>
+          <Field
+            className="input"
+            name="passwordConfirm"
+            type="password"
+            autoComplete="off"
+          />
+          <p>{touched.password && errors.password}</p>
+        </div>
         <button type="submit" className="btn">
           {" "}
           Register
@@ -60,6 +70,9 @@ export default withFormik({
     username: Yup.string().required("Username is required"),
     password: Yup.string()
       .min(6)
+      .required("Password is required"),
+    passwordConfirm: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
       .required("Password is required")
   }),
   handleSubmit(values, formikBag) {
