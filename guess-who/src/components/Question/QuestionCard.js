48210e923445d2c6783c7fe92e113ef437662aba
Taran <thetaranneeld@gmail.com>
2019-08-29 13:29:53 -0600

    test

--- guess-who/src/components/Question/QuestionCard.js
+++ guess-who/src/components/Question/QuestionCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, Image, Icon } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import "./question.scss";
+import "./Question.scss";
 
 const QuestionCard = props => {
 
